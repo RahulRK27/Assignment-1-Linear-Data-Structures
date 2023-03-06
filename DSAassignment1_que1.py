a8643f0e31b95a14bed8217eef14e962ee1db319
rahul khapre <121964920+RahulRK27@users.noreply.github.com>
2023-03-06 11:36:35 +0530

    Add files via upload

---
+++ DSAassignment1_que1.py
@@ -0,0 +1,18 @@
+# Write a program to find all pairs of an integer array whose sum is equal to a given number
+
+
+def sum_pairs(arr , num , sum):
+    for i in range(num):
+        for j in range(i+1 , num):
+            if arr[i] + arr[j] == sum:
+                print("(",arr[i],",",arr[j],")",sep = "")
+                
+                
+                
+# drive the code
+arr = [5, 6, 4, 5, 1, 9, 3, 7, 2, 8]
+num = len(arr)
+sum = 10
+
+
+sum_pairs(arr,num,sum)
